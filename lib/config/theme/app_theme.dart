0ea5d37169ed5e31e7a624fdd4b3c61f44cba3ab
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-10-26 14:33:27 +0700

    style: Change app theme color scheme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 ThemeData myTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0f8f7e)),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xFF003366),
+    primary: const Color(0xFF003366),
+    secondary: const Color(0xFF06FFE1),
+    // background: Colors.white,
+  ),
   useMaterial3: true,
   fontFamily: 'Poppins',
 );
