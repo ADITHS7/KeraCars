67023a2f5cb790fda4536497f544c9e0456dfabd
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-10-30 10:24:04 +0700

    refactor: Use bloc instead of flutter_bloc in bloc class

--- lib/features/auth/presentation/blocs/remote/login/login_bloc.dart
+++ lib/features/auth/presentation/blocs/remote/login/login_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
+import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:keracars_app/core/network/resources/data_state.dart';
 import 'package:keracars_app/features/auth/domain/entities/entities.dart';
 import 'package:keracars_app/features/auth/domain/usecases/usecases.dart';
